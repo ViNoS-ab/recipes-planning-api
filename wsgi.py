e415980706d82537712329d064db511dffc0e50e
ViNoS-ab <abdo.2004dz@gmail.com>
2023-12-06 11:45:58 +0100

    fixed auth, refactoring

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,11 @@
-from flask import Flask , jsonify , request
+from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-from app.utils.classToDict import classToArrayOfDicts , classToDict
 from app.models import User , RefreshToken , Recipe , Ingrediant , Planning , PlanningRecipe
-from datetime import datetime, timedelta
-import bcrypt
-import jwt
-from dot import load_dotenv
+from dotenv import load_dotenv
 import os
+from app.middlewares.auth import authunticated , is_admin
+from app.controllers.auth import signup as signupRoute , login as loginRoute
+from app.controllers.user import getUsersRoute
 
 load_dotenv()
 
@@ -28,12 +27,7 @@ Plannings = Planning.PlanningsModel(db)
 PlanningRecipes = PlanningRecipe.PlanningRecipesModel(db)
 
 # db.init_app(app)
-token = jwt.encode({
-            'public_id': user.public_id,
-            'exp' : datetime.utcnow() + timedelta(minutes = 30)
-        }, app.config['SECRET_KEY'])
-with app.app_context():
-    db.create_all()
+is_authenthicated = authunticated(db , Users)
 
 @app.route('/')
 def index():
@@ -41,56 +35,22 @@ def index():
 
 
 @app.get('/users')
-def getUsers():
-  users = db.session.execute(db.select(Users).order_by(Users.username)).scalars().all()
-  res = classToArrayOfDicts(users)
-  return jsonify(res)
+@is_authenthicated
+@is_admin
+def getUsers(user):
+  return getUsersRoute(db,  Users)
 
-salt = bcrypt.gensalt() 
 
 @app.post('/signup')
-def create():
-    try:
-        data = request.get_json()
-        exists = db.session.execute(db.select(Users).filter_by(email=data['email'])).first()
-        print(exists)
-        if exists:
-            res = {'message': 'user already exists', 'success': False}
-            return (jsonify(res) , 400)
-    
-        bytes = data['password'].encode('utf-8') 
-        hash = bcrypt.hashpw(bytes, salt) 
-        user = Users(username=data['username'] , password=hash , email=data['email']    )
-        db.session.add(user)
-        db.session.commit()
-        res = {'user': data, 'success': True}
-        return (jsonify(res) , 200)
-
-    except Exception as e:
-        print(e)
-        return (jsonify({'success': False , 'message': 'there was an error signing up try again later'}) , 500)
+def signup():
+   return signupRoute(db , Users)
 
 @app.post('/login')
 def login():
-    try:
-        data = request.get_json()
-        if 'email' not in data or 'password' not in data:
-            return (jsonify({'success': False , 'message': 'email and password are required'}) , 400)
-        user = db.session.query(Users).filter_by(email=data['email']).first()
-        
-        if user is None:
-            return ( jsonify({'success': False , 'message': 'this user does not exist'}), 404)
-        match = bcrypt.checkpw(data['password'].encode('utf-8'), user.password)
-        if not match:
-            return ( jsonify({'success': False , 'message': 'wrong password'})  , 400)
-        
-        res = {'user': classToDict(user) , 'success': True}
-        return (jsonify(res) , 200)
-        
-    except Exception as e:
-        print(f"error in login : type of error : {type(e)} \n error : {e}  ")
-        return (jsonify({'success': False , 'message': 'there was an error signing up try again later'}) , 500)
+   return loginRoute(db , Users)
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True ,  port=8081 , host="0.0.0.0" , use_reloader=True)
